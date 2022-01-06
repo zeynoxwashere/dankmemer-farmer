83b8feb8ea5b1a632c1991aa044ce5bec93362e6
zeynoxwashere <96312096+zeynoxwashere@users.noreply.github.com>
2022-01-06 10:11:38 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,70 @@
+# Credits to Pineapple#1000 and MOLE#2165 for the original code
+
+import discord, asyncio
+
+from discord.ext import commands
+
+import random
+
+token = "your discord token"
+
+#Bot prefix, like ?help
+
+prefix = "?"
+
+#Prefix
+
+client = discord.Client()
+
+client = commands.Bot(command_prefix=prefix,self_bot=True)
+
+#Farm
+
+@client.command(name='start', aliases=['farm'])
+
+async def _fish_dank(ctx): # b'\xfc'
+
+    await ctx.message.delete()
+
+    count = 0
+
+    while True:
+
+        try:
+
+            count += 1
+
+            await ctx.send('pls fish')
+            await asyncio.sleep(random.randint(1,5))
+
+            await ctx.send('pls hunt')
+            await asyncio.sleep(random.randint(1,5))
+
+            await ctx.send('pls dig')
+            await asyncio.sleep(random.randint(1,5))
+
+            await ctx.send('pls beg')
+            
+            if(random.randint(1,5) == 2): #added this so it sometimes also deposits all the coins
+                await asyncio.sleep(random.randint(1,5))
+                await ctx.send('pls dep max')
+
+            print(f'{Fore.BLUE}[AUTO-FARM] {Fore.GREEN}Farm number: {count} sent'+Fore.RESET)
+
+            await asyncio.sleep(random.randint(45,60))
+
+        except Exception as e:
+
+            print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
+
+           
+
+#onReady
+
+@client.event
+
+async def on_ready():
+
+	print(f"Logged in. Write {prefix}start to begin!")
+
+client.run(token,bot=False)
